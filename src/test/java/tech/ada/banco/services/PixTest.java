59e7a7a709033f016a8925443d6204abb5187c08
jackson-veloso <jackson.veloso@hotmail.com>
2023-02-13 20:18:28 -0300

    add ideias testes para class Pix

--- src/test/java/tech/ada/banco/services/PixTest.java
+++ src/test/java/tech/ada/banco/services/PixTest.java
@@ -1,7 +1,42 @@
 package tech.ada.banco.services;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PixTest {
+    @Test
+    void testPixSaldoPositivo() {
 
+    }
+
+    @Test
+    void testPixSaldoPositivoFracionado() {
+
+    }
+
+    @Test
+    void testPixSaldoTotal() {
+
+    }
+
+    @Test
+    void testPixSaldoInsuficiente() {
+
+    }
+
+    @Test
+    void testPixSaldoNegativo() {
+
+    }
+
+    @Test
+    void testPixContaOrigemInvalida(){
+
+    }
+
+    @Test
+    void testPixContaDestinoInvalida(){
+
+    }
 }
